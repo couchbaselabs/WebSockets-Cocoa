042a95b9d78b7f92645ce5a8090e0076ef3962e7
Jens Alfke <jens@couchbase.com>
2013-09-10 10:26:05 -0700

    Initial Commit

---
+++ WebSocket/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WebSocket
+//
+//  Created by Jens Alfke on 9/10/13.
+//  Copyright (c) 2013 Couchbase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
